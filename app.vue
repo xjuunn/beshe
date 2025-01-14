802abd79075f650845507ebda02667328f5eadf3
Junhsiun <xjuunn@gmail.com>
2025-01-14 19:20:25 +0800

    干净的项目

--- app.vue
+++ app.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app-content" class="select-none relative overflow-x-hidden scrollbar-thin">
-    <NuxtLayout @toTop="toTop">
+    <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
 <script setup lang="ts">
-function toTop() {
-  let appContent = document.getElementById('app-content');
 
-  appContent?.scrollTo({
-    top: 0,
-    behavior: "smooth" // 平滑滚动
-  })
-
-}
 </script>
 <style>
 .glass {
