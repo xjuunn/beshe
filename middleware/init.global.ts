b6c58e54523e72107138c5ec4c0b4f43e6967b7c
Junhsiun <xjuunn@gmail.com>
2025-02-12 20:39:29 +0800

    权限拦截页面

--- middleware/init.global.ts
+++ middleware/init.global.ts
@@ -1,10 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     if (to.fullPath.includes('admin')) {
         setTimeout(() => {
-            let user;
-            if (import.meta.client) user = JSON.parse(localStorage.getItem('user') + '');
-            if (!user) return navigateTo('/login');
-            if (user.roleId != 1) return navigateTo('/noPermissions')
+            if (import.meta.client) {
+                let user = JSON.parse(localStorage.getItem('user') + '');
+                if (!user) return navigateTo('/login');
+                if (user.roleId != 1) return navigateTo('/noPermissions')
+            }
         }, 100);
     }
 });
--- pages/noPermissions.vue
+++ pages/noPermissions.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    无权限
+  <div class="flex items-center justify-center flex-col w-full h-screen">
+    <Icon name="mingcute:safe-flash-fill" class="mt-[-80px] animate-pulse" size="7rem" style="color: #4F46E5;"></Icon>
+    <div class="font-bold text-4xl text-center mt-5">
+      无权访问当前页面
+    </div>
+    <div class="text-lg text-center opacity-70 mt-3">
+      你没有权限访问此页面，请检查权限设置或返回。
+    </div>
+    <div class="mt-8">
+      <button @click="navigateTo('/')"
+        class="btn btn-soft px-8 py-3 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-105">
+        返回
+      </button>
+    </div>
   </div>
 </template>
 
