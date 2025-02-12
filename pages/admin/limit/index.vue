c7c2e464c856ac5e4c6d38e0fb60af6f9d9a09b8
Junhsiun <xjuunn@gmail.com>
2025-02-12 18:02:37 +0800

    限购管理

---
+++ pages/admin/limit/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <title>限购管理</title>
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin'
+})
+useBreadcrumbsStore().setBreadcrumbs([
+  {name: '仪表盘', path: '/admin'},
+  {name: '限购管理', path: '/admin/limit'}
+])
+</script>
+
