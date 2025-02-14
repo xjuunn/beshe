68322479a816aef9e8465c7f770bbeaa533b1bcd
Junhsiun <xjuunn@gmail.com>
2025-02-14 22:35:48 +0800

    订单列表

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -36,12 +36,14 @@
           <div class="flex mt-2">
             <div class="flex-1">
               <span>金额: </span>
-              <span class="text-xl font-semibold text-primary me-4">¥{{ item.products?.price }}</span>
+              <span class="text-xl font-semibold text-primary me-4">¥{{ (item.products?.price ?? 1) * item.quantity
+                }}</span>
               <p class="text-base-content/50 text-xs"> {{ item.products?.createTime }}</p>
             </div>
             <div class="card-actions justify-end flex items-end pe-2">
               <div class="join">
-                <button class="btn join-item btn-primary btn-sm" @click="() => { }">下单</button>
+                <button class="btn join-item btn-primary btn-sm"
+                  @click="() => { showCreateOrderModal = true; orderitem = item }">下单</button>
                 <button class="btn btn-info btn-sm join-item"
                   @click="() => { navigateTo('/products/' + item.productId) }">商品详情</button>
                 <button class="btn btn-error btn-sm join-item" @click="btnDel(item.id)">删除</button>
@@ -64,7 +66,39 @@
         </div>
       </template>
     </ModalVue>
-
+    <ModalVue title="创建订单" :show="showCreateOrderModal" @update:show="(e) => { showCreateOrderModal = e }">
+      <template #body>
+        <div class="mt-2">
+          <div class="w-full flex justify-center">
+            <img class="w-[50%]" :src="Product.getProductCover((orderitem?.products?.cover ?? '').split(',')[0])"
+              alt="图片">
+          </div>
+          <div class="m-3">
+            <p class="font-bold">商品名称: &nbsp;&nbsp; {{ orderitem?.products?.name }}</p>
+            <p class="mt-1">商品信息: &nbsp;&nbsp; {{ orderitem?.products?.info }}</p>
+            <p v-show="orderitem?.products?.model" class="mt-1">型号: &nbsp;&nbsp; {{ orderitem?.products?.model }}</p>
+            <p class="mt-1">价格:&nbsp;&nbsp;{{ orderitem?.products?.price ?? 1 }} &nbsp; * &nbsp; {{ orderitem?.quantity
+              ?? 1 }} &nbsp; = &nbsp;{{ (orderitem?.products?.price ?? 1) * (orderitem?.quantity ?? 1) }}</p>
+          </div>
+        </div>
+      </template>
+      <template #bottom>
+        <div class="join">
+          <button @click="showCreateOrderModal = false;" class="btn btn-sm join-item btn-ghost">取消</button>
+          <button @click="btnPay" class="btn btn-sm join-item btn-primary">支付</button>
+        </div>
+      </template>
+    </ModalVue>
+    <ModalVue title="支付信息" :show="showLoadingModal" @update:show="(e) => { showLoadingModal = e }">
+      <template #body>
+        <div v-show="!ordercreateresult" class="text-center m-10">
+          <div class="loading loading-infinity loading-lg"></div>
+        </div>
+        <div v-show="ordercreateresult" class="font-bold text-2xl text-center m-10">
+          {{ ordercreateresult }}
+        </div>
+      </template>
+    </ModalVue>
   </div>
 </template>
 
@@ -75,12 +109,17 @@ definePageMeta({
 })
 import * as Cart from '../../api/cart';
 import * as Product from '../../api/products';
+import * as Order from '../../api/order';
 import { useThemeStore } from '../../stores/useThemeStore';
 let listdata: Ref<Cart.CartDTO[]> = ref([]);
 let isloading = ref(true);
 let showModal = ref(false);
 let delId = ref(-1);
 let showSaveBtn: Ref<any> = ref({})
+let showCreateOrderModal = ref(false);
+let orderitem: Ref<Cart.CartDTO | undefined> = ref();
+let showLoadingModal = ref(false);
+let ordercreateresult = ref('');
 onMounted(() => {
   initList();
 })
@@ -112,6 +151,36 @@ async function delCart() {
   } else {
     createToast(data.message, { type: 'error', style: 'soft', icon: 'mdi:error' })
   }
+}
+async function btnPay() {
+  showCreateOrderModal.value = false;
+  ordercreateresult.value = '';
+  showLoadingModal.value = true;
+  let { data: data1 } = await Order.add({
+    order: {
+      userId: user.value.id,
+      productId: orderitem.value?.id ?? -1,
+      status: 1,
+      totalPrice: (orderitem.value?.quantity ?? 1) * (orderitem.value?.products?.price ?? 1),
+    },
+    orderDetailsList: [
+      {
+        price: orderitem.value?.products?.price ?? 1,
+        productId: orderitem.value?.products?.id ?? -1,
+        quantity: orderitem.value?.quantity ?? 1
+      }
+    ]
+  });
+  let { data: data2 } = await Cart.del(orderitem.value?.id ?? -1);
+  if (data2.code == 200) {
+    initList();
+  }
+
+  showLoadingModal.value = true;
+  ordercreateresult.value = data1.data;
+  setTimeout(() => {
+    showLoadingModal.value = false;
+  }, 3000);
 
 }
 </script>
