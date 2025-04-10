47a7d2a3f86555081b0c8d286275471914fde7a4
hhsmantou <109007845+hhsmantou@users.noreply.github.com>
2025-04-10 09:04:59 +0800

    提交

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # 基于Web的校园百货零售网站设计与实现 前端项目
 
 后端仓库: [基于Web的校园百货零售网站设计与实现 后端项目](https://github.com/hhsmantou/xiaoxixi)
-
-[论文](./content/论文.md)
--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
-dotenv.config(); // 显式加载 .env 文件
+dotenv.config(); // 显式加载 ..env 文件
 
 const prisma = new PrismaClient();
 export default prisma;
